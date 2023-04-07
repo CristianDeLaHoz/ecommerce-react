7ab1b59ba05477d35892394e5ff7b0ab752204d1
CristianDeLaHoz <cristiandelahoz@hotmail.com>
2023-04-07 12:32:14 -0300

    entregando

--- js/app.js
+++ js/app.js
@@ -1,28 +1,12 @@
-import React, { useState } from 'react';
-import './App.css';
+import React from 'react';
+import Navbar from './Navbar';
 
 function App() {
-  const [products, setProducts] = useState([
-    { id: 1, name: "Collar para perro", price: 10 },
-    { id: 2, name: "Juguete para gato", price: 5 },
-    { id: 3, name: "Comida para perro", price: 20 },
-  ]);
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Pet Shop</h1>
-      </header>
-      <main>
-        <h2>Productos</h2>
-        <ul>
-          {products.map((product) => (
-            <li key={product.id}>
-              {product.name} - ${product.price}
-            </li>
-          ))}
-        </ul>
-      </main>
+    <div>
+      <Navbar />
+      <h1>Bienvenido a la tienda de mascotas</h1>
+      <p>Aquí encontrarás todo lo que necesitas para tu mascota</p>
     </div>
   );
 }
